f5c6fe99b6c916d9e9618ab37f322b7068d57dc4
Marcelo19AL <t033300420@unitru.edu.pe>
2024-01-11 17:09:09 -0500

    A

--- resources/views/transporte/plantillaT.blade.php
+++ resources/views/transporte/plantillaT.blade.php
@@ -106,15 +106,9 @@
                     </div>
                 </div>
 
-
-                <!-- Sidebar Menu -->
-                <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
-                        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
-                        <!-- Mesas -->
                         @if (auth()->user()->rol == 'administrador')
                             <!--REPARTIDORES -->
                             <li class="nav-item">
